7d76b1dedfec412ddca6723471900d4856128cc0
Keith Lin <keith@relational.com.au>
2017-05-08 16:46:47 +0930

    fix the broken validation

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -3208,8 +3208,7 @@ angular.module('schemaForm')
 ]);
 
 angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse', 'sfSelect',
-  function(sfValidator, $parse, sfSelect) {
-
+  function (sfValidator, $parse, sfSelect) {
     return {
       restrict: 'A',
       scope: false,
@@ -3217,7 +3216,7 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
       // the parsed value, ex. a number instead of a string
       priority: 500,
       require: 'ngModel',
-      link: function(scope, element, attrs, ngModel) {
+      link: function (scope, element, attrs, ngModel) {
         // We need the ngModelController on several places,
         // most notably for errors.
         // So we emit it up to the decorator directive so it can put it on scope.
@@ -3227,17 +3226,17 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
         var form = scope.$eval(attrs.schemaValidate);
 
         if (form.copyValueTo) {
-          ngModel.$viewChangeListeners.push(function() {
+          ngModel.$viewChangeListeners.push(function () {
             var paths = form.copyValueTo;
-            angular.forEach(paths, function(path) {
+            angular.forEach(paths, function (path) {
               sfSelect(path, scope.model, ngModel.$modelValue);
             });
           });
-        };
+        }
 
         // Validate against the schema
-        var validate = function(viewValue) {
-          //Still might be undefined
+        var validate = function (viewValue) {
+          // Still might be undefined
           if (!form) {
             return viewValue;
           }
@@ -3247,14 +3246,15 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
             return viewValue;
           }
 
-          var result = {'valid': true}
+          var result = sfValidator.validate(form, viewValue);
+
           // Since we might have different tv4 errors we must clear all
           // errors that start with tv4-
           Object.keys(ngModel.$error)
-              .filter(function(k) { return k.indexOf('tv4-') === 0; })
-              .forEach(function(k) { ngModel.$setValidity(k, true); });
-          ngModel.$setValidity('jsExpression' , true);
-          ngModel.$setValidity('remoteValidation' , true);
+              .filter(function (k) { return k.indexOf('tv4-') === 0; })
+              .forEach(function (k) { ngModel.$setValidity(k, true); });
+          ngModel.$setValidity('jsExpression', true);
+          ngModel.$setValidity('remoteValidation', true);
 
           if (!result.valid) {
             // it is invalid, return undefined (no model update)
@@ -3283,7 +3283,6 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
               return viewValue;
             }
           }
-
         };
 
         // Custom validators, parsers, formatters etc
@@ -3291,18 +3290,18 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
           form.ngModel(ngModel);
         }
 
-        ['$parsers', '$viewChangeListeners', '$formatters'].forEach(function(attr) {
+        ['$parsers', '$viewChangeListeners', '$formatters'].forEach(function (attr) {
           if (form[attr] && ngModel[attr]) {
-            form[attr].forEach(function(fn) {
+            form[attr].forEach(function (fn) {
               ngModel[attr].push(fn);
             });
           }
         });
 
-        ['$validators', '$asyncValidators'].forEach(function(attr) {
+        ['$validators', '$asyncValidators'].forEach(function (attr) {
           // Check if our version of angular has validators, i.e. 1.3+
           if (form[attr] && ngModel[attr]) {
-            angular.forEach(form[attr], function(fn, name) {
+            angular.forEach(form[attr], function (fn, name) {
               ngModel[attr][name] = fn;
             });
           }
@@ -3315,21 +3314,20 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
         // But we do use one custom validator in the case of Angular 1.3 to stop the model from
         // updating if we've found an error.
         if (ngModel.$validators) {
-          ngModel.$validators.schemaForm = function() {
-            //console.log('validators called.')
+          ngModel.$validators.schemaForm = function () {
+            // console.log('validators called.')
             // Any error and we're out of here!
-            return !Object.keys(ngModel.$error).some(function(e) { return e !== 'schemaForm' && e !== 'jsExpression' && e !== 'remoteValidation';});
+            return !Object.keys(ngModel.$error).some(function (e) { return e !== 'schemaForm' && e !== 'jsExpression' && e !== 'remoteValidation'; });
           };
         }
 
         var schema = form.schema;
 
         // A bit ugly but useful.
-        scope.validateField =  function(formName) {
-
+        scope.validateField = function (formName) {
           // If we have specified a form name, and this model is not within
           // that form, then leave things be.
-          if(formName != undefined && ngModel.$$parentForm.$name !== formName) {
+          if (formName != undefined && ngModel.$$parentForm.$name !== formName) {
             return;
           }
 
@@ -3346,7 +3344,6 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
           // since modelValue might be empty and validating just that
           // might change an existing error to a "required" error message.
           if (ngModel.$setDirty) {
-
             // Angular 1.3+
             ngModel.$setDirty();
             ngModel.$setViewValue(ngModel.$viewValue);
@@ -3358,16 +3355,15 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
             ngModel.$setValidity('tv4-302', true); // hotfix for not reset required validation
             if (form.required && ngModel.$isEmpty(ngModel.$modelValue)) {
               ngModel.$setValidity('tv4-302', false);
-            } else if (form.jsExpressionResult === false ) {
+            } else if (form.jsExpressionResult === false) {
               ngModel.$setValidity('jsExpression', false);
-            } else if(form.jsExpressionResult){
+            } else if (form.jsExpressionResult) {
               ngModel.$setValidity('jsExpression', true);
-            } else if(form.remoteValidationResult === false){
+            } else if (form.remoteValidationResult === false) {
               ngModel.$setValidity('remoteValidation', false);
-            } else if(form.remoteValidationResult){
+            } else if (form.remoteValidationResult) {
               ngModel.$setValidity('remoteValidation', true);
             }
-
           } else {
             // Angular 1.2
             // In angular 1.2 setting a viewValue of undefined will trigger the parser.
@@ -3379,11 +3375,11 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
         // TODO: This seems like a hack, but it's what is happening on the schemaform demo page? first was previously scope.firstDigest
         // It solves the problem of forms validating when a condition passes and new fields are shown though
         var first = true;
-        ngModel.$formatters.push(function(val) {
+        ngModel.$formatters.push(function (val) {
           // When a form first loads this will be called for each field.
           // we usually don't want that.
-          if (ngModel.$pristine  && first &&
-              (!scope.options || scope.options.validateOnRender !== true))  {
+          if (ngModel.$pristine && first &&
+              (!scope.options || scope.options.validateOnRender !== true)) {
             first = false; // added with fix
             return val;
           }
@@ -3392,11 +3388,11 @@ angular.module('schemaForm').directive('schemaValidate', ['sfValidator', '$parse
         });
 
         // Listen to an event so we can validate the input on request
-        scope.$on('schemaFormValidate', function(event, formName) {
+        scope.$on('schemaFormValidate', function (event, formName) {
           scope.validateField(formName);
         });
 
-        scope.schemaError = function() {
+        scope.schemaError = function () {
           return error;
         };
       }
